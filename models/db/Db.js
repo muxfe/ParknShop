b9718b325f561dcf0b7ca438f9d4d28f8618af5e
kyer <grzpjdyc1653@126.com>
2015-10-05 18:17:57 +0800

    database operator module

---
+++ models/db/Db.js
@@ -0,0 +1,231 @@
+/*
+ * 数据库操作类
+ * Author: x-web
+ * Date: 5/10/2015
+ */
+
+var url = require('url');
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+
+var settings = require('./settings');
+
+var db = mongoose.connect(settings.URL);
+/* 数据库需要安全认证时，用下面的连接 */
+// mongoose.connect('mongodb://' + settings.USERNAME + ':' + settings.PASSWORD + '@'+settings.HOST + ':'+settings.PORT + '/' + settings.DB + '');
+
+var Db = {
+
+	delete: function (obj, req, res, logMsg) {
+		var params = url.parse(req.url, true);
+		obj.remove({ _id: params.query.uid }, function (err, result) {
+			if (err) {
+				res.end(err);
+			} else {
+				console.log(logMsg + ' success!');
+				res.end('success');
+			}
+		});
+	},
+
+	findAll: function (obj, req, res, logMsg) {
+		obj.find({}, function (err, result) {
+			if (err) {
+				res.next(err);
+			} else {
+				console.log(logMsg + ' success!');
+				return res.json(result);
+			}
+		});
+	},
+
+	findOne: function (obj, req, res, logMsg) {
+		var params = url.parse(req.url, true);
+		var currentId = (params.query.uid).split('.')[0];
+		obj.findOne({ _id: currentId }, function (err, result) {
+			if (err) {
+				res.next(err);
+			} else {
+				console.log(logMsg + ' success!');
+				return res.json(result);				
+			}
+		});
+	},
+
+	updateOneById: function (obj, req, res, logMsg) {
+		var params = url.parse(req.url, true);
+		var condition = { _id: params.query.uid };
+		res.body.updateDate = new Date();
+		var update = { $set: req.body };
+		obj.update(condition, update, function (err) {
+			if (err) {
+				res.end(err);
+			} else {
+				console.log(logMsg + ' success!');
+				res.end('success');
+			}
+		});
+	},
+
+	addOne: function (obj, req, res, logMsg) {
+		var newObj = new obj(req.body);
+		newObj.save(function (err) {
+			if (err) {
+				res.end(err);
+			} else {
+				console.log(logMsg + ' success!');
+				res.end('success');
+			}
+		});
+	},
+
+	pagination: function (obj, req, res, coditions) {
+		var params = url.parse(req.url, true),
+			startNum = (params.query.currentPage - 1) * params.query.limit + 1,
+			currentPage = Number(params.query.currentPage),
+			limit = Number(params.query.limit),
+			pageInfo = null;
+
+		var query = null;
+		if (conditions && conditions.length > 1) {
+			query = obj.find().or(conditions);
+		} else if (conditions) {
+			query = obj.find(conditions);
+		} else {
+			query = obj.find({});
+		}
+
+		query.sort({ "date": -1 });
+		query.exec(function (err, docs) {
+			if (err) {
+				console.error(err);
+			} else {
+				pageInfo = {
+					"totalItems": docs.length,
+					"currentPage": currentPage,
+					"limit": limit,
+					"startNum": Number(startNum)
+				};
+
+				return res.json({ 
+					docs: docs.slice(startNum - 1, startNum + limit - 1),
+					pageInfo: pageInfo
+				});
+			}
+		});
+	},
+
+	getPaginationResult: function (obj, req, res, q, filed) {
+		var searchKey = req.query.searchKey,
+			page = parseInt(req.query.page),
+			limit = parseInt(req.query.limit);
+
+		if (!page) {
+			page = 1;
+		}
+		if (!limit) {
+			limit = 15;
+		}
+
+		var order = req.query.order,
+			sq = {},
+			Str = '',
+			A = 'problemID',
+			B = 'asc';
+
+		if (order) {
+			Str = order.split('_');
+			A = Str[0];
+			B = Str[1];
+			sq[A] = B;
+		} else {
+			sq.date = -1;
+		}
+
+		var startNum = (page - 1) * limit,
+			resultList = null,
+			resultNum = 0;
+
+		if (q && q.length > 1) {
+			resultList = obj.find().or(q, filed).sort(sq).skip(startNum).limit(limit);
+			resultNum = obj.find().or(q, filed).count();
+		} else {
+			resultList = obj.find(q, filed).sort(sq).skip(startNum).limit(limit);
+			resultNum = obj.find(q, filed).count();
+		}
+
+		var datasInfo = {
+			docs: resultList,
+			pageInfo: {
+				"totalItems": resultNum,
+				"currentPage": page,
+				"limit": limit,
+				"startNum": startNum,
+				"searchKey": searchKey
+			}
+		};
+
+		return datasInfo;
+	},
+
+	getDatasByParam: function (obj, req, res, q) {
+		var order = req.query.order,
+			limit = parseInt(req.query.limit),
+			sq = {},
+			Str = '',
+			A = 'problemID',
+			B = 'asc';
+
+		if (order) {
+			Str = order.split('_');
+			A = Str[0];
+			B = Str[1];
+			sq[A] = B;
+		} else {
+			sq.date = -1;
+		}
+
+		if (!limit) {
+			return obj.find(q).sort(sq);
+		} 
+		
+		return obj.find(q).sort(sq).skip(0).limit(limit);
+	},
+
+	getKeyArrayByTokenId: function (tokenId) {
+		return Db.decrypt(tokenId, settings.encrypt_key).split('$');
+	},
+
+	getCount: function (obj, req, res, conditions) {
+		obj.count(coditions, function (err, count) {
+			if (err) {
+				console.error(err);
+			} else {
+				return res.json({
+					count: count 
+				});
+			}
+		});
+	},
+
+	encrypt: function (data, key) {
+		var cipher = crypto.createCipher("bf", key);
+		var newPwd = '';
+		newPwd += cipher.update(data, "utf8", "hex");
+		newPwd += cipher.final("hex");
+
+		return newPwd;
+	},
+
+	decrypt: function (data, key) {
+		var decipher = crypto.createDecipher("bf", key);
+		var oldPwd = '';
+		oldPwd += decipher.update(data, "hex", "utf8");
+		oldPwd += decipher.final("utf8");
+
+		return oldPwd;
+	}
+
+};
+
+module.exports = Db;
